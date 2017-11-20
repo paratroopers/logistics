cf77eb29cd90f7c5531024a274c62d4143f9fd44
handy.zhang <handy.zhang@akmii.com>
2017-11-20 15:35:44 +0800

    基础配置

--- src/api/common-api.ts
+++ src/api/common-api.ts
@@ -1,10 +1,10 @@
 import {DemoRequest} from "./model/request/common-request";
+import {NaRequest,NaResponse} from "../util/common"
 import {Request} from '../util/request';
 
 export class DemoAPI {
     static async GetDemo(data?: DemoRequest) {
         let url: string = "sockjs-node/info";
-        return new Request<NaRequest,
-            NaResponse>().get(url, data);
+        return new Request<NaRequest, NaResponse>().get(url, data);
     }
 }
--- src/api/model/common-model.ts
+++ src/api/model/common-model.ts
@@ -1,20 +1,3 @@
-/* Demo Enum*/
-export enum DemoEnum{
-    demo = 0
-}
-
-/* Demo Interface*/
-export interface DemoInterface {
-    demo: any;
-}
-
-/* Demo Class*/
-export class DemoClass {
-    demo: any;
-}
-
-/* 公共 ====================================================================*/
-
 /* 国际化*/
 export interface AppLocaleStatic {
     antd?: Object;
