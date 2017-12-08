79a77d5a4e03bd8e23624172132fd0fd4959099c
handy.zhang <handy.zhang@akmii.com>
2017-12-08 20:44:37 +0800

    流程

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -7,7 +7,7 @@ import {NaConsultingCenterPage} from "./index/na-consulting-center-page";
 import {NaCompanyProfilePage} from "./index/na-company-profile-page";
 import {NaVIPConsigneeAddressPage}from "./vip/na-vip-consignee-address-page";
 import {NaVIPWelcomePage}from "./vip/na-vip-welcome-page";
-
+import {NaMobileTabBar} from "./demo/na-mobile-tabbar";
 export {
     NaDemoPage,
     NaHomePage,
@@ -17,5 +17,6 @@ export {
     NaCompanyProfilePage,
     NaVIPCenterPage,
     NaVIPConsigneeAddressPage,
-    NaVIPWelcomePage
+    NaVIPWelcomePage,
+    NaMobileTabBar
 }
