a1ba9ec54eecbf1dc35ff1ae33fae912b8947c3f
handy.zhang <handy.zhang@akmii.com>
2017-12-08 18:25:34 +0800

    流程

--- src/page/index/na-process-demo-page.tsx
+++ src/page/index/na-process-demo-page.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import {Component} from "react";
 import {withRouter} from "react-router";
-import {Layout} from "antd";
+import {Layout, Steps, Icon} from "antd";
+import {NaContext} from '../../util/common';
 const {Content} = Layout;
+const {Step} = Steps;
 
 interface NaProcessDemoPageProps {
 
@@ -11,6 +13,22 @@ interface NaProcessDemoPageProps {
 interface NaProcessDemoPageProps {
 
 }
+
+const data = [
+    {
+        title: "免费注册获得仓库信息",
+        cover: "http://www.famliytree.cn/icon/process-demo_1.jpg"
+    }, {
+        title: "将需要仓储、转运的物品发往您的“专属仓库地址”",
+        cover: "http://www.famliytree.cn/icon/process-demo_2.jpg"
+    }, {
+        title: "物品打包后，提交打包申请",
+        cover: "http://www.famliytree.cn/icon/process-demo_3.jpg"
+    }, {
+        title: "填写物品海关申报，并提交发运",
+        cover: "http://www.famliytree.cn/icon/process-demo_4.jpg"
+    }
+];
 
 @withRouter
 export class NaProcessDemoPage extends Component<NaProcessDemoPageProps, NaProcessDemoPageProps> {
@@ -18,13 +36,21 @@ export class NaProcessDemoPage extends Component<NaProcessDemoPageProps, NaProce
         super(props, context)
     }
 
+    renderStep() {
+        return data.map(function (item, index) {
+            const des = <img src={item.cover}/>;
+            return <Step key={index} status="process" title={item.title} description={des}/>
+        })
+    }
 
 
     render() {
         const topThis = this;
-        return <Layout style={{minHeight: '100%'}}>
+        return <Layout style={{minHeight: '100%', backgroundColor: '#FFF'}}>
             <Content>
-                流程演示
+                <Steps direction="vertical">
+                    {topThis.renderStep()}
+                </Steps>
             </Content>
         </Layout>;
     }
