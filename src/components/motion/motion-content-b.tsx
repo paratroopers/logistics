d80cacc449e128c47ec52dfe76766f74c19d792c
handy.zhang <handy.zhang@akmii.com>
2017-12-09 15:58:26 +0800

    content

--- src/components/motion/motion-content-b.tsx
+++ src/components/motion/motion-content-b.tsx
@@ -29,33 +29,33 @@ export default class MotionContentBControl extends Component<MotionContentBContr
         const oneAnim = {y: '+=30', opacity: 0, type: 'from', ease: 'easeOutQuad'};
         const blockArray = [
             {
-                icon: 'https://zos.alipayobjects.com/rmsportal/ScHBSdwpTkAHZkJ.png',
-                title: '企业资源管理',
+                icon: 'http://www.famliytree.cn/icon/motion-content-b-a.png',
+                title: '运输中、物流',
                 content: '云资源集中编排、弹性伸缩、持续发布和部署，高可用及容灾。'
             },
             {
-                icon: 'https://zos.alipayobjects.com/rmsportal/NKBELAOuuKbofDD.png',
-                title: '云安全',
+                icon: 'http://www.famliytree.cn/icon/motion-content-b-b.png',
+                title: '包裹、发货',
                 content: '按金融企业安全要求打造的完整云上安全体系，全方位保障金融应用及数据安全。'
             },
             {
-                icon: 'https://zos.alipayobjects.com/rmsportal/xMSBjgxBhKfyMWX.png',
-                title: '云监控',
+                icon: 'http://www.famliytree.cn/icon/motion-content-b-c.png',
+                title: '配载、装车',
                 content: '分布式云环境集中监控，统一资源及应用状态视图，智能分析及故障定位。'
             },
             {
-                icon: 'https://zos.alipayobjects.com/rmsportal/MNdlBNhmDBLuzqp.png',
-                title: '移动',
+                icon: 'http://www.famliytree.cn/icon/motion-content-b-d.png',
+                title: '拆包裹、取件',
                 content: '一站式移动金融APP开发及全面监控；丰富可用组件，动态发布和故障热修复。'
             },
             {
-                icon: 'https://zos.alipayobjects.com/rmsportal/UsUmoBRyLvkIQeO.png',
-                title: '分布式中间件',
+                icon: 'http://www.famliytree.cn/icon/motion-content-b-e.png',
+                title: '签收、审批',
                 content: '金融级联机交易处理中间件，大规模分布式计算机，数万笔/秒级并发能力，严格保证交易数据统一性。'
             },
             {
-                icon: 'https://zos.alipayobjects.com/rmsportal/ipwaQLBLflRfUrg.png',
-                title: '大数据',
+                icon: 'http://www.famliytree.cn/icon/motion-content-b-f.png',
+                title: '安全、保障',
                 content: '一站式、全周期大数据协同工作平台，PB级数据处理、毫秒级数据分析工具。'
             },
         ];
