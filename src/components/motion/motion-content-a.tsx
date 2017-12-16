7f4b3373f4abeb677e710b86dbf929c7f9164c4c
home <1005824013@qq.com>
2017-12-16 18:08:39 +0800

    部分调整

--- src/components/motion/motion-content-a.tsx
+++ src/components/motion/motion-content-a.tsx
@@ -4,7 +4,6 @@ import {Component} from "react";
 import QueueAnim from 'rc-queue-anim';
 import TweenOne from 'rc-tween-one';
 import ScrollAnim from 'rc-scroll-anim';
-import {Tag} from 'antd';
 import {isNullOrUndefined} from "util";
 const OverPack = ScrollAnim.OverPack;
 
@@ -24,12 +23,12 @@ export default class MotionContentAControl extends Component<MotionContentAContr
 
     getBlockChildren = (item, i) => {
         return <li key={i}>
-            <div className="icon">
-                <i className={"na-iconfont " + item.icon}></i>
+            <div className="line"></div>
+            <div className="icon"
+                 style={{border: item.type === 0 ? "2px solid #c2c2c2" : "2px dashed #c2c2c2"}}>
+                <i className={"iconfont " + item.icon} style={{color: item.type === 0 ? "#1890FF" : "#e65922"}}></i>
             </div>
-            <Tag color={item.type === 0 ? "blue" : "orange"}>{item.type === 0 ? "客户" : "公司"}</Tag>
             <h3>{item.title}</h3>
-            {/*<p>{item.content}</p>*/}
         </li>;
     }
 
@@ -38,32 +37,32 @@ export default class MotionContentAControl extends Component<MotionContentAContr
         const {props, props: {className}} = topThis;
         const dataSource = [
             {
-                icon: "na-icon-jisuanqilishuai",
+                icon: "icon-jisuanqilishuai",
                 title: '费用估算',
                 type: 0
             },
             {
-                icon: 'na-icon-jijianfasong',
+                icon: 'icon-jijianfasong',
                 title: '客户发货',
                 type: 0
             },
             {
-                icon: 'na-icon-cangkucangchu',
+                icon: 'icon-cangkucangchu',
                 title: '入库操作',
                 type: 1
             },
             {
-                icon: 'na-icon-qianshoushenpitongguo',
+                icon: 'icon-qianshoushenpitongguo',
                 title: '客户确认',
                 type: 0
             },
             {
-                icon: 'na-icon-baoguofahuo',
+                icon: 'icon-baoguofahuo',
                 title: '合并打包',
                 type: 1
             },
             {
-                icon: 'na-icon-yunshuzhongwuliu',
+                icon: 'icon-yunshuzhongwuliu',
                 title: '国际快递',
                 type: 1
             }
@@ -75,12 +74,24 @@ export default class MotionContentAControl extends Component<MotionContentAContr
                  className={`content-template-wrapper ${isNullOrUndefined(className) ? "motion-content-a" : className}-wrapper`}>
                 <OverPack
                     className={`content-template ${isNullOrUndefined(className) ? "motion-content-a" : className}`}>
-                    <TweenOne animation={{y: '+=30', opacity: 0, type: 'from'}}
+                    <TweenOne className="with-line" animation={{y: '+=30', opacity: 0, type: 'from'}}
                               component="h1"
                               key="h1"
                               reverseDelay={300}>
                         一键式服务流程
                     </TweenOne>
+                    <TweenOne animation={{y: '+=30', opacity: 0, type: 'from'}}
+                              component="h4"
+                              key="h4"
+                              reverseDelay={300}>
+                        操作更便捷，发货更省心
+                    </TweenOne>
+                    <QueueAnim className="content-tag" type="bottom" key="tag" leaveReverse>
+                        <div className="item" key={0} style={{color: "#1890FF"}}><i
+                            className="iconfont icon-yonghu1"></i><span>客户完成</span></div>
+                        <div className="item" key={1} style={{color: "#e65922"}}><i
+                            className="iconfont icon-weibiaoti-"></i><span>大陆完成</span></div>
+                    </QueueAnim>
                     <QueueAnim component="ul" type="bottom" key="block" leaveReverse>
                         {listChildren}
                     </QueueAnim>
