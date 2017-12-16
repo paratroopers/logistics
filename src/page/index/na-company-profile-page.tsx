e51de3ad4f21624f3146c423b977bb2c2f838477
home <1005824013@qq.com>
2017-12-16 22:49:51 +0800

    部分调整

--- src/page/index/na-company-profile-page.tsx
+++ src/page/index/na-company-profile-page.tsx
@@ -30,8 +30,8 @@ export class NaCompanyProfilePage extends Component<NaCompanyProfilePageProps, N
             </Col>
             <Col lg={1} xl={1}></Col>
             <Col xs={24} sm={24} md={24} lg={12} xl={12}>
-                <Row style={{margin: '24px 0'}}>
-                    <h2 style={{fontWeight: 800}}>关于我们</h2>
+                <Row className="about-content" style={{margin: '24px 0'}}>
+                    <h2>关于我们</h2>
                     <p>Mainload物流网隶属于（电子商务有限公司）旗下，是一家专业从事货物仓储及国际包裹转运的服务商。</p>
                     <p> Mainload创建于2003年，专业服务于国内外贸公司及海外华人，为用户提供货物在线仓 储、验货、清点、打包、报关、运输等一条龙服务。</p>
                     <p> Mainload为客户提供全方位和量身定制的转运服务，只为给客户带来最优质的物流方案、 最优惠的转运价格、最棒的国际转运体验。</p>
@@ -40,14 +40,14 @@ export class NaCompanyProfilePage extends Component<NaCompanyProfilePageProps, N
                 </Row>
             </Col>
             <Col span={24}>
-                <Row style={{margin: '24px 0'}}>
-                    <h2 style={{fontWeight: 800}}>我们的服务</h2>
+                <Row className="about-content" style={{margin: '24px 0'}}>
+                    <h2>我们的服务</h2>
                     <p>美幻国际物流网致力为客户提供更省心，更便捷的仓储物流服务。通过整合上下游的资源，提供多渠道的物流方式，使您的货物更快速、更经济的方式送达目的地。</p>
                 </Row>
             </Col>
             <Col span={24}>
-                <Row style={{margin: '24px 0'}}>
-                    <h2 style={{fontWeight: 800}}>企业宗旨:诚信、高效、创新</h2>
+                <Row className="about-content" style={{margin: '24px 0'}}>
+                    <h2>企业宗旨:诚信、高效、创新</h2>
                     <p>
                         美幻网的核心所在，只适当收取相关服务费用，绝不私吞广大用户的一针一线。北京时间17：00前提交的打包订单当天封装完毕。服务的同时我们会不断的丰富自己，改正自身的缺点，推出更多、更好、更便捷的服务理念，绝不辜负您对“美幻网”的信任。</p>
                 </Row>
@@ -57,7 +57,7 @@ export class NaCompanyProfilePage extends Component<NaCompanyProfilePageProps, N
 
     render() {
         const topThis = this;
-        return <Layout style={{minHeight: '100%', backgroundColor: '#FFF'}}>
+        return <Layout style={{minHeight: '100%', backgroundColor: '#FFF'}} className="company-page">
             <Content>
                 <Row style={{marginBottom: 24}} className="page-title-company-image">
                 </Row>
