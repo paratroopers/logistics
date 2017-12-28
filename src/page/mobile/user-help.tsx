b3a4c06a15b265515c4e1a1f3236b0ef19cd2dc4
home <1005824013@qq.com>
2017-12-28 21:40:39 +0800

    手机客服样式

--- src/page/mobile/user-help.tsx
+++ src/page/mobile/user-help.tsx
@@ -15,10 +15,12 @@ export class MbUserHelp extends React.Component<UserHelpProps, UserHelpStates> {
         return <Layout className="user-help-layout">
             <Layout.Content className="user-help-layout-content">
                 <Row className="wechat">
-                    <Col span={24}><p className="title">您的专属客服</p></Col>
-                    <Col span={24}><a className="number" href="tel:400-100-2013">400-100-2013</a></Col>
-                    <Col span={24}><img src="http://www.famliytree.cn/icon/wx_ewm.jpg"/></Col>
-                    <Col span={24}><p className="tip">扫一扫上面的二维码图案，加客服微信</p></Col>
+                    <Row>
+                        <Col span={24}><p className="title">您的专属客服</p></Col>
+                        <Col span={24}><a className="number" href="tel:400-100-2013">400-100-2013</a></Col>
+                        <Col span={24}><img src="http://www.famliytree.cn/icon/wx_ewm.jpg"/></Col>
+                        <Col span={24}><p className="tip">扫一扫上面的二维码图案，加客服微信</p></Col>
+                    </Row>
                 </Row>
             </Layout.Content>
         </Layout>
