042a1b2262764494b99792633957a3db02e913d6
Evanxuke <xuke_break@163.com>
2018-01-18 22:52:32 +0800

    Edit by Evan： 步骤 图标 样式调优

--- src/page/web/member/member-welcome-page.tsx
+++ src/page/web/member/member-welcome-page.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {Component} from "react";
 import {withRouter} from "react-router";
-import {Row, Col, Avatar, Card} from "antd";
+import {Row, Col, Avatar, Card, Icon} from "antd";
 
 import {FormMessageList} from '../../../components/form/form-message-list';
 import {FormSteps} from '../../../components/form/form-steps';
@@ -25,22 +25,25 @@ export class MemberWelcomePage extends Component<MemberWelcomePageProps, MemberW
             {
                 status: 'finish',
                 title: '用户无需下单直接发货到大陆的专属仓库',
-                icon: 'smile'
+                icon: <Icon style={{color: '#079bef'}} type="user"/>
             },
             {
                 status: 'finish',
                 title: '大陆收货确认，帮您在我的仓库中生成订单',
-                icon: 'smile'
+                icon: <Icon style={{color: '#079bef'}} type="check-square"/>
             },
             {
                 status: 'finish',
                 title: '用户在我的仓库中进行合并打包',
-                icon: 'smile'
+                icon: <i style={{
+                    color: '#079bef', fontSize: '24px',
+                    marginRight: '0px'
+                }} className="iconfont icon-shoujianchenggong"></i>
             },
             {
                 status: 'finish',
                 title: '待客服确认，仓库人员打包之后，用户选择渠道付款发货',
-                icon: 'smile'
+                icon: <Icon style={{color: '#079bef'}} type="check"/>
             }]
     }
 
@@ -116,7 +119,7 @@ export class MemberWelcomePage extends Component<MemberWelcomePageProps, MemberW
             <Col span={8} className="welcome-content welcome-content-right">
                 <Card className="content-card" title="温馨提示：操作流程">
                     <Card.Grid className="content-card-grid">
-                        <FormSteps direction="vertical" size="small" itemClassStyle={{height: '40px'}}
+                        <FormSteps direction="vertical" size="small" itemClassStyle={{height: '80px'}}
                                    data={this.getStrpsData()}></FormSteps>
                     </Card.Grid>
                 </Card>
