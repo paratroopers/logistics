cf98045051e695ad59f1ff5f7cef1eb0f5ef4514
enzo.shi <jingshierr@126..com>
2018-01-19 17:56:16 +0800

    demo

--- src/page/demo/demo.tsx
+++ src/page/demo/demo.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import {withRouter} from 'react-router';
+import { Input } from 'antd';
+import { Menu, Dropdown, Icon } from 'antd';
 
 interface DemoStates {
 
@@ -14,8 +16,61 @@ export class DemoPage extends React.Component<DemoProps, DemoStates> {
     constructor(props) {
         super(props);
     }
+    objCustom:CustomObj = {i:"11"};
 
     render() {
-        return <div>Demo</div>
+
+        return <div></div>
+    }
+}
+
+interface CustomObj{
+
+}
+
+interface CustomInputProps{
+
+}
+
+interface  CustomInputStates{
+
+}
+
+
+export class CustomInput extends React.Component<CustomInputProps, CustomInputStates> {
+
+    render() {
+      return  <div>11</div>
+    }
+}
+
+interface CustomDropProps{
+}
+
+interface  CustomDropStates{
+
+}
+export  class  CustomDropdown extends React.Component<CustomDropProps, CustomDropStates> {
+
+     menu:any = (
+        <Menu>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
+            </Menu.Item>
+            <Menu.Item>
+                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
+            </Menu.Item>
+        </Menu>
+    );
+
+    render(){
+        return  <Dropdown overlay={this.menu}>
+            <a className="ant-dropdown-link" href="#">
+                Hover me <Icon type="down" />
+            </a>
+        </Dropdown> ;
     }
 }
