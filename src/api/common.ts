8c12b73ed9403a66a4505e4d1d4fdde773f977fa
enzo.shi <jingshierr@126..com>
2018-01-20 23:10:48 +0800

    api

--- src/api/common.ts
+++ src/api/common.ts
@@ -1,6 +1,6 @@
 export class CommonAPI {
    // static  uatURL = "http://localhost:8090/_api/ver(1.0)/";
-   static  baseURL = "http://localhost:8090/_api/ver(1.0)/";
-   // static  baseURL = "http://localhost:8090/_api/ver(1.0)/";
+   static  baseURL = "http://famliytree.cn/_api/ver(1.0)/";
+   // static  baseURL = "http://famliytree.cn/_api/ver(1.0)/";
 
 }
