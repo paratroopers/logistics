85f03c80d26fa70993528b593270d90f94137294
Evanxuke <xuke_break@163.com>
2018-02-02 00:33:43 +0800

    Edit by Evan： 合并打包 折叠面板

--- src/components-v1/form-order-addressee.tsx
+++ src/components-v1/form-order-addressee.tsx
@@ -15,39 +15,51 @@ export class FormOrderAddressee extends React.Component<FormOrderAddresseeProps,
         return <Button type="dashed">添加联系人</Button>
     }
 
+    renderRow(){
+
+    }
+
     render() {
         const formItemLayout = {labelCol: {span: 10}, wrapperCol: {span: 14}};
         return <FormSettingGroup size={16} title={"收件人基本信息"} span={24} footer={this.renderFooter()}>
             <Form>
                 <Row>
-                    <Col span={6}>
+                    <Col>
                         <Form.Item label={'收件人姓名'} {...formItemLayout} required>
                             <Input placeholder={'请输入'}></Input>
                         </Form.Item>
                     </Col>
-                    <Col span={9}>
+                </Row>
+                <Row>
+                    <Col>
                         <Form.Item label={'收件国家'} {...formItemLayout} required>
                             <Input placeholder={'请输入'}></Input>
                         </Form.Item>
                     </Col>
-                    <Col span={9}>
+                </Row>
+                <Row>
+                    <Col>
                         <Form.Item label={'收件城市'} {...formItemLayout} required>
                             <Input placeholder={'请输入'}></Input>
                         </Form.Item>
                     </Col>
                 </Row>
                 <Row>
-                    <Col span={6}>
+                    <Col>
                         <Form.Item label={'邮编'} {...formItemLayout} required>
                             <Input placeholder={'请输入'}></Input>
                         </Form.Item>
                     </Col>
-                    <Col span={8}>
+                </Row>
+                <Row>
+                    <Col>
                         <Form.Item label={'电话'} {...formItemLayout} required>
                             <Input placeholder={'请输入'}></Input>
                         </Form.Item>
                     </Col>
-                    <Col span={10}>
+                </Row>
+                <Row>
+                    <Col>
                         <Form.Item label={'公司名或税号'} {...formItemLayout} required>
                             <Input placeholder={'请输入'}></Input>
                         </Form.Item>
--- src/components-v1/form-setting-group.tsx
+++ src/components-v1/form-setting-group.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Row, Col, Spin} from 'antd';
+import {Row, Col, Spin, Collapse} from 'antd';
 
 export interface FormSettingGroupProps {
     title?: string;
@@ -17,21 +17,22 @@ export class FormSettingGroup extends React.Component<FormSettingGroupProps, For
     render() {
         const {props: {title, size, span, footer, loading}} = this;
         return <Spin spinning={(loading === true)}>
-            <Row className="form-control-group">
-                <Col className="form-control-title" style={{fontSize: size}} span={24}>
-                    <span className="sign">{title}</span>
-                </Col>
-                {!loading ?
-                    [
-                        <Col key="children" span={span}>
-                            {this.props.children}
-                        </Col>,
-                        <Col key="footer" span={span} className="form-control-button">
-                            {footer}
-                        </Col>
-                    ] : null
-                }
-            </Row>
+            <Collapse className="form-control-group" defaultActiveKey={['1']}>
+                <Collapse.Panel key="1" header={title} style={{fontSize: size}} showArrow={false} className="panel">
+                    <Row>
+                        {!loading ?
+                            [
+                                <Col key="children" span={span}>
+                                    {this.props.children}
+                                </Col>,
+                                <Col key="footer" span={span} className="form-control-button">
+                                    {footer}
+                                </Col>
+                            ] : null
+                        }
+                    </Row>
+                </Collapse.Panel>
+            </Collapse>
         </Spin>
     }
 }
