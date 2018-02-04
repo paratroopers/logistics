f18fe0c842cd9cefedb43882864a91182cb26191
enzo.shi <jingshierr@126..com>
2018-02-04 23:12:49 +0800

    New changelist

--- src/model/api.ts
+++ src/model/api.ts
@@ -9,7 +9,6 @@ export namespace APINameSpace {
         // static  uatURL = "http://localhost:8090/_api/ver(1.0)/";
          //static baseURL = "http://www.famliytree.cn/_api/ver(1.0)/";
        static  baseURL = "http://localhost:8090/_api/ver(1.0)/";
-
     }
 
     //endregion
