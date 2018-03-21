2ec84ecf38c0dfc461becef09a7f10f42a7aa19b
home <1005824013@qq.com>
2018-03-21 23:50:59 +0800

    ...

--- src/page-v1/demo.tsx
+++ src/page-v1/demo.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import {withRouter} from 'react-router';
-import {Row, Col, Popover, Icon, Badge, Card, Tabs} from 'antd';
-import {FormMessageList} from '../components-v1/form-message-list';
-const TabPane = Tabs.TabPane;
+import {Row, Col,Form} from 'antd';
+import {extendConfigurationFile} from "tslint/lib/configuration";
+const FormItem=Form.Item;
 
 interface DemoStates {
 
@@ -13,23 +13,27 @@ interface DemoProps {
 }
 
 @withRouter
-export default class DemoPage extends React.Component<DemoProps, DemoStates> {
+export class DemoPage extends React.Component<DemoProps, DemoStates> {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return <div className="demo-page">
-            <Card style={{maxWidth: 900}} bodyStyle={{padding:0}}>
-                <Tabs tabBarExtraContent={<a style={{margin:"0 32px",height:55,display:"flex",alignItems:"center"}}>查看更多</a>} tabBarStyle={{marginBottom:4}} size="large">
-                    <TabPane tab="物流信息" key="0">
-                        <FormMessageList layoutText={true} tagStatus={true}></FormMessageList>
-                    </TabPane>
-                    <TabPane tab="消息通知" key="1">
-                        <FormMessageList layoutText={true} tagStatus={true} isSystem={true}></FormMessageList>
-                    </TabPane>
-                </Tabs>
-            </Card>
-        </div>
+        return <Row className="demo-page">
+            <FormMessageManager></FormMessageManager>
+        </Row>
     }
 }
+
+class FormMessageManager extends React.Component<DemoProps, DemoStates> {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <Form>
+
+        </Form>
+    }
+}
+export default Form.create<any>()(FormMessageManager);
