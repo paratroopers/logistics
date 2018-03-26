c0653d5f8c55eb731c7e173ea392cb41b38e9243
enzo.shi <jingshierr@126..com>
2018-03-26 23:13:27 +0800

    返回控件

---
+++ src/components-v1/form-button-cancel.tsx
@@ -0,0 +1,37 @@
+/**
+ * 返回按钮
+ * Created by Handy
+ * */
+import * as React from 'react';
+import {Button} from "antd";
+import {hashHistory} from 'react-router';
+
+
+interface FormButtonCancelProps {
+    url?:string;
+}
+
+interface FormButtonCancelPropsStates {
+}
+
+export class FormButtonCancel extends React.Component<FormButtonCancelProps, FormButtonCancelPropsStates> {
+    constructor(props, content) {
+        super(props, content);
+    }
+
+    returnUrl() {
+        if(this.props.url !== '')
+        {
+            hashHistory.push(this.props.url);
+
+        }else {
+            hashHistory.goBack();
+        }
+
+    }
+
+    render() {
+
+        return <Button type="primary" onClick={this.returnUrl.bind(this)}>取消</Button>;
+    }
+}
