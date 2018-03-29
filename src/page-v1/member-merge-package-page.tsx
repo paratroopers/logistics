d42c7eb0b32555d157ad11659c77f7db381b1f0f
handy.zhang <handy.zhang@akmii.com>
2018-03-29 23:39:46 +0800

    bug

--- src/page-v1/member-merge-package-page.tsx
+++ src/page-v1/member-merge-package-page.tsx
@@ -150,16 +150,6 @@ class MemberMergePackagePage extends React.Component<MemberMergePackagePageProps
         return <Layout className="merge-package-page view-content-page">
             <ContentHeaderControl title="待打包"></ContentHeaderControl>
             <Layout.Content>
-                <Row justify="start" type="flex" style={{margin: '10px 0px 10px 0px'}}>
-                    <Col span={8} offset={18}>
-                        <div className="view-content-page-header-button">
-                            <Button type="primary" style={{marginRight: "10px"}} onClick={this.onSubmit.bind(this)}>确认合并打包</Button>
-                            <Button type="primary" onClick={() => {
-                                hashHistory.goBack();
-                            }}>取消</Button>
-                        </div>
-                    </Col>
-                </Row>
                 <FormOrderInfo data={orderInfo}></FormOrderInfo>
                 <FormOrderRelation data={data}></FormOrderRelation>
                 <FormOrderAddressee form={form}></FormOrderAddressee>
@@ -168,6 +158,16 @@ class MemberMergePackagePage extends React.Component<MemberMergePackagePageProps
                     this.setState({channelList: channelData});
                 }}></FormOrderChannel>
                 <FormRemarks form={form} title={"打包要求"} fieldName="CustomerMark"></FormRemarks>
+                <Row justify="start" type="flex" style={{margin: '10px 0px 10px 0px'}}>
+                    <Col>
+                        <div className="view-content-page-header-button">
+                            <Button type="primary" style={{marginRight: "10px"}} onClick={this.onSubmit.bind(this)}>确认合并打包</Button>
+                            <Button type="primary" onClick={() => {
+                                hashHistory.goBack();
+                            }}>取消</Button>
+                        </div>
+                    </Col>
+                </Row>
             </Layout.Content>
         </Layout>
     }
